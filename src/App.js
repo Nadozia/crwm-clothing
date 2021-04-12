6adabb8b2b791c8420f05833679a130bb1c2dcc8
Lewix Wong <qpmz9713@gmail.com>
2021-04-12 21:52:07 +0800

    update to navigate with router on hats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
+
 import './App.css';
 import HomePage from './pages/hompage/homepage.component'
 
+const HatsPage=()=>(
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
+  
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop/hats' component={HatsPage}/>
     </div>
     
   );
