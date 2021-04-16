2c6a9f5ab41096ca6f1af061c64b6254c24ff230
Lewix Wong <qpmz9713@gmail.com>
2021-04-16 13:19:33 +0800

    updated the add to cart feature

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,7 +5,9 @@ const CollectionItem=({id, name, imageUrl, price})=>(
     <div className='collection-item'>
         <div 
             className='image' 
-            style={{backgroundImage:`url(${imageUrl})`}}/>
+            style={{backgroundImage:`url(${imageUrl})`}}>
+            <div className='addItem'>ADD TO CART</div>
+        </div>
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{`$${price}`}</span>
